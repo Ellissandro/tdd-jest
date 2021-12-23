c047272bb8e4b543b5e29dea15a7e9814c63744a
Elissandro Silva Soares <elissandro.soares@wakke.co>
2021-12-22 23:01:56 -0300

    ensure CheckLastEventStatus returns status active when now is equals to event and time

--- src/testes/check-last-event-status.spec.ts
+++ src/testes/check-last-event-status.spec.ts
@@ -10,7 +10,7 @@ class CheckLastEventStatus {
         const event = await this.loadLastEventRepository.loadLastEvent({ groupId });
         if (event === undefined) return { status: 'done' };
         const now = new Date();
-        return event.endDate > now ? { status:'active' } : { status:'inReview' };
+        return event.endDate >= now ? { status:'active' } : { status:'inReview' };
     }
 }
 
@@ -74,6 +74,17 @@ describe('CheckLastEventStatus', () => {7
         const { sut, loadLastEventRepository } = makeSut();
         loadLastEventRepository.output = {
             endDate: new Date(new Date().getTime() + 1)
+        };
+
+        const eventStatus = await sut.perform({ groupId: groupId });
+
+        expect(eventStatus.status).toBe('active');
+    })
+
+    it('should return status active when now is equal to event and time', async () => {
+        const { sut, loadLastEventRepository } = makeSut();
+        loadLastEventRepository.output = {
+            endDate: new Date()
         };
 
         const eventStatus = await sut.perform({ groupId: groupId });
